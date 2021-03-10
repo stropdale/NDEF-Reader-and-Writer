63fd10c9687584bbc2a22ac05eaaa5a2ce62daa8
Richard Stockdale <richardstockdale@mac.com>
2021-03-10 08:24:57 +0000

    It lights up!

--- NTAG Read Write/CmdsViewController.swift
+++ NTAG Read Write/CmdsViewController.swift
@@ -17,17 +17,17 @@ class CmdsViewController: UIViewController, NFCTagReaderSessionDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func led1ButtonTapped(_ sender: Any) {
-//        NTAG_I2C_LIB.sharedInstance()?.initSession({ (nil) in
-//            self.connected()
-//        }, onFailure: { (error) in
-//            print("failed to get the session")
-//        })
-
+        //        NTAG_I2C_LIB.sharedInstance()?.initSession({ (nil) in
+        //            self.connected()
+        //        }, onFailure: { (error) in
+        //            print("failed to get the session")
+        //        })
+        
         connect() // MINE
     }
     
@@ -40,10 +40,10 @@ class CmdsViewController: UIViewController, NFCTagReaderSessionDelegate {
             print("Not connected")
             return
         }
-
+        
         let sramSize = 64
         let dataTx = NSMutableData(length: sramSize)!
-
+        
         // TODO: Should do the foloowing in swift, but I cant work out the new API here. Drop in to NS
         let led = "L1".data(using: .ascii)
         let ledData = NSData(data: led!)
@@ -56,158 +56,84 @@ class CmdsViewController: UIViewController, NFCTagReaderSessionDelegate {
         session = NFCTagReaderSession(pollingOption: .iso14443, delegate: self)
         session?.begin()
     }
-
+    
     // MARK: - TagReaderSessionDelegate Methods
     func tagReaderSessionDidBecomeActive(_ session: NFCTagReaderSession) {
         //infoTextView.text = "Session did become active: \(session.description)"
         print("Session did become active: \(session.description)")
     }
-
+    
     func tagReaderSession(_ session: NFCTagReaderSession, didInvalidateWithError error: Error) {
         let errorCode = (error as NSError).code
         if errorCode == 200 { return } // This is user terminating the read
-
-//        infoTextView.text = error.localizedDescription
-//        infoTextView.textColor = .red
+        
+        //        infoTextView.text = error.localizedDescription
+        //        infoTextView.textColor = .red
         session.invalidate()
     }
-
+    
     func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
         guard let tag = tags.first else {
             return
         }
         
-
-        
-        session.connect(to: tag) { (error) in
-            
-            
-            
-            print("Tag connected: \(tag.isAvailable)")
-        }
-
-
-
-        
+        if case let NFCTag.miFare(tag) = tag {
+            session.connect(to: tags.first!) { (error) in
+                if let error = error {
+                    self.infoTextView.text = error.localizedDescription
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.printTagDescription(tag: tag)
+                }
+                
+                sendCmd(tag: tag)
+            }
+        }  
     }
     
-//    private func printTagDescription(tag: NFCTag) {
-//
-//        let info = """
-//Tag detected.
-//
-//MIFARE Tag Identifier: \(tag.asNFCMiFareTag.identifier)
-//MIFARE Tag Family: \(tag.asNFCMiFareTag.mifareFamily)
-//MIFARE Tag Historial Bytes: \(tag)
-//MIFARE Tag Type: \(tag)
-//MIFARE Tag description: \(tag)
-//"""
-//
-//    }
+    private func printTagDescription(tag: NFCMiFareTag) {
+        
+        let info = """
+Tag detected.
+
+MIFARE Tag Identifier: \(tag.identifier)
+MIFARE Tag Family: \(tag.mifareFamily)
+MIFARE Tag Historial Bytes: \(tag.historicalBytes)
+MIFARE Tag Type: \(tag.type)
+MIFARE Tag description: \(tag.description)
+"""
+        print(info)
+        infoTextView.text = info
+    }
 }
 
+private func sendCmd(tag: NFCMiFareTag) {
+    let sramSize = 64
+    let dataTx = NSMutableData(length: sramSize)!
+    
+    // TODO: Should do the foloowing in swift, but I cant work out the new API here. Drop in to NS
+    let led = "L1".data(using: .ascii)
+    let ledData = NSData(data: led!)
+    let ledBytes = ledData.bytes
+    dataTx.replaceBytes(in: NSMakeRange(sramSize - 4, 2), withBytes: ledBytes)
+    
+    write(tag: tag, dataTx: dataTx)
+}
 
-//extension CmdsViewController: NFCNDEFReaderSessionDelegate {
-//    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
-//        // NOT USED HERE
-//    }
-//
-//    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
-//        let errorCode = (error as NSError).code
-//        if errorCode == 200 { return } // This is user terminating the read
-//
-//        infoTextView.text = error.localizedDescription
-//        infoTextView.textColor = .red
-//        session.invalidate()
-//    }
-//
-//    func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
-//        if tags.count > 1 {
-//            infoTextView.text = "Detected \(tags.count) tags. Move any other tags away and try again."
-//            infoTextView.textColor = .red
-//            session.invalidate()
-//
-//            return
-//        }
-//
-//        guard let tag = tags.first else { return }
-//
-//        tag.queryNDEFStatus(completionHandler: { (status, capacity, error) in
-//            if let error = error {
-//                self.infoTextView.text = "Error getting tag info: \(error.localizedDescription)"
-//                self.infoTextView.textColor = .red
-//                session.invalidate()
-//                return
-//            }
-//
-//            self.infoTextView.text = "Tag capacity: \(capacity) bytes"
-//            self.handleTag(tag: tag, status: status)
-//        })
-//    }
-//
-//    private func handleTag(tag: NFCNDEFTag, status: NFCNDEFStatus) {
-//        switch status {
-//        case .notSupported:
-//            self.infoTextView.text.append("\n !!! THIS TAG IS NOT SUPPORTED")
-//            self.infoTextView.textColor = .red
-//        case .readWrite:
-//            self.infoTextView.text.append("\n Tag is writable...\n")
-//            sendCmd(tag: tag)
-//        case .readOnly:
-//            self.infoTextView.text.append("\n !!! THIS TAG IS NOT WRITABLE")
-//            self.infoTextView.textColor = .red
-//        @unknown default:
-//            self.infoTextView.text.append("\n !!! UNKNOWN TAG TYPE")
-//            self.infoTextView.textColor = .red
-//        }
-//    }
-//
-//    private func sendCmd(tag: NFCNDEFTag) {
-//        session?.connect(to: tag, completionHandler: { (error) in
-//            if let error = error {
-//                let fail = "Connect to tag failed \(error.localizedDescription)"
-//                self.session?.alertMessage = fail
-//                self.infoTextView.text.append("\n \(fail)")
-//                self.infoTextView.textColor = .red
-//
-//                return
-//            }
-//
-//            let sramSize = 64
-//            let dataTx = NSMutableData(length: sramSize)!
-//
-//            // TODO: Should do the foloowing in swift, but I cant work out the new API here. Drop in to NS
-//            let led = "L1".data(using: .ascii)
-//            let ledData = NSData(data: led!)
-//            let ledBytes = ledData.bytes
-//            dataTx.replaceBytes(in: NSMakeRange(sramSize - 4, 2), withBytes: ledBytes)
-//
-//            self.write(tag: tag, dataTx: dataTx)
-//
-//
-//
-//        })
-//    }
-//
-//    private func write(tag: NFCNDEFTag, dataTx: NSMutableData) {
-//        var charArray = [CUnsignedChar](repeating: 0, count: 3)
-//        charArray[0] = 0xA6
-//        charArray[1] = 0xF0
-//        charArray[2] = 0xF0 + 0x0F
-//
-//        let cmd = NSMutableData(bytes: charArray, length: charArray.count)
-//        cmd.append(dataTx as Data)
-//
-//
-//        // TODO: Send the command
-//        if let t = tag as? NFCMiFareTag {
-//            t.sendMiFareCommand(commandPacket: cmd as Data) { (data, error) in
-//
-//
-//
-//
-//            }
-//        }
-//
-//    }
-//}
+private func write(tag: NFCMiFareTag, dataTx: NSMutableData) {
+    var charArray = [CUnsignedChar](repeating: 0, count: 3)
+    charArray[0] = 0xA6
+    charArray[1] = 0xF0
+    charArray[2] = 0xF0 + 0x0F
+    
+    let cmd = NSMutableData(bytes: charArray, length: charArray.count)
+    cmd.append(dataTx as Data)
+    
+    tag.sendMiFareCommand(commandPacket: cmd as Data) { (data, error) in
+        if let error = error {
+            print(error.localizedDescription)
+        }
+    }
+}
